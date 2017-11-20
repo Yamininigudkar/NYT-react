e439972f87d9f768b9a1556366a33a8336c8e150
Yamini <yaminipuranik@gmail.com>
2017-11-20 10:20:40 -0600

    deleted unused elements

--- client/src/pages/Articles/Article.js
+++ client/src/pages/Articles/Article.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
+
 import ArticleBtn from "../../components/ArticleBtn";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, FormBtn } from "../../components/Form";
 
 class Articles extends Component {
   state = {
